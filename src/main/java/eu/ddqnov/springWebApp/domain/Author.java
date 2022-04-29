cec3fe02ed51e121aaa5ab0377ba8cb5d0666bd5
Deyan Pakov <deian.pakov@smartwitness.com>
2022-04-29 16:49:00 +0300

    equals, hasCode, toString to models JPA

--- src/main/java/eu/ddqnov/springWebApp/domain/Author.java
+++ src/main/java/eu/ddqnov/springWebApp/domain/Author.java
@@ -56,15 +56,27 @@ public class Author {
     }
 
     @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Author author = (Author) o;
+
         return Objects.equals(id, author.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return id != null ? id.hashCode() : 0;
     }
 }
