a1f9046de5cb9d705d3ff3bce65f7f67faa09b65
Deyan Pakov <deian.pakov@smartwitness.com>
2022-04-29 16:43:14 +0300

    POJO with JPA and Relations

---
+++ src/main/java/eu/ddqnov/springWebApp/SpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package eu.ddqnov.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+
+}
